3d3229fe7649d8e74c975e1d45c7438ce30f6cd5
Jairos <jairos@gmail.com>
2025-10-02 23:04:59 -0500

    ProyectoSuperusuario

---
+++ JascEcommerce/views.py
@@ -0,0 +1,3 @@
+from  django.shortcuts import HttpResponse, render 
+def inicio(request):
+    return render(request, 'index.html', {})
---
+++ auths/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Auth
+admin.site.register(Auth,)
+# Register your models here.
+
+
+# Register your models here.
